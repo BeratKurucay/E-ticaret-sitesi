d266d4dcf9404ec5d586f565ac635d7e41893b2a
Jacob <55778775+Yakup24@users.noreply.github.com>
2025-04-10 15:33:40 +0300

    Add files via upload

---
+++ E-Ticaret-Projesi php web/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PikPazar</title>
+    <link rel="stylesheet" type="text/css" href="E-Ticaret.css">
+</head>
+<body>
+    <?php include("partials/header.php"); ?>
+    <?php include("partials/menu.php"); ?>
+</body>
+</html>
---
+++ E-Ticaret-Projesi php web/partials/header.php
@@ -0,0 +1,7 @@
+<div id="markaFont">
+    <h1 id="marka">PikPazarıM</h1>
+    <p id="slogan">Hızlı ve Anında Alışveriş</p>
+    <div class="arama-cubugu">
+        <input type="text" class="arama-cubugu-input" placeholder="Ürün ara...">
+    </div>
+</div>
